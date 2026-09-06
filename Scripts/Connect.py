978f441c6ab53a54b9f8461a5897418682a0021d
aleonevenerius <alexandreleonevenerius@gmail.com>
2026-09-06 16:14:06 -0300

    New functions

--- Scripts/Connect.py
+++ Scripts/Connect.py
@@ -1,12 +1,15 @@
 import psycopg2
-def __init__():
-    connecting = psycopg2.connect(database = "cafe_db", host = "localhost", user = "postgres", password = "7898", port = "5432")
-       
-    #print(connecting.info)
-    #print(connecting.status)
-    cursor = connecting.cursor()
+
+connecting = psycopg2.connect(database = "cafe_db", host = "localhost", user = "postgres", password = "7898", port = "5432")
+   
+#print(connecting.info)
+#print(connecting.status)
+cursor = connecting.cursor()
+
+def ToShowOrder():
     cursor.execute('SELECT * FROM db_order')
-    #print(cursor.fetchall())
+
+def ToShowCafeProducts():
+    cursor.execute('SELECT * FROM db_products')
     
-if __init__():
-    print("Working!")
+def ToCreateOrder():
